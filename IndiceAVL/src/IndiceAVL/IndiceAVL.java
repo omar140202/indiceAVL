f93217278d36fa25a19bd4f74e7bad3737bd038f
DanielRoa <danbron523@gmail.com>
2020-07-01 21:02:15 -0500

    segundo boceto de la interfaz de usuario

--- IndiceAVL/src/IndiceAVL/IndiceAVL.java
+++ IndiceAVL/src/IndiceAVL/IndiceAVL.java
@@ -7,7 +7,8 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,10 +18,14 @@ import javax.swing.WindowConstants;
 
 public class IndiceAVL extends JFrame implements ActionListener{
     
+    ArbolAVL arbol;
+    Cursor materias, estudiantes;
+    
     JButton botonCrear = new JButton("Crear Arbol");
     JButton botonIngresoEstudiante = new JButton("Ingresar Estudiante");
     JButton botonIngresoMateria = new JButton("Ingresar Materia");
     JButton botonConsultar = new JButton("Consultar");
+    JButton botonDibujar = new JButton("Dibujar Abol");
     
     JLabel texto1 = new JLabel("Datos Estudiantes.");
     JLabel texto2 = new JLabel("Datos Materias.");
@@ -29,20 +34,24 @@ public class IndiceAVL extends JFrame implements ActionListener{
     JLabel texto5 = new JLabel("Materias:");
     JLabel texto6 = new JLabel("Codigo Materia: ");
     JLabel texto7 = new JLabel("Nombre Materia: ");
+    JLabel texto8 = new JLabel("Didite el codigo del estudiante");
     
     JTextField codEst = new JTextField();
     JTextField nomEst = new JTextField();
     JTextField matEst = new JTextField();
     JTextField codMat = new JTextField();
     JTextField nomMat = new JTextField();
+    JTextField campoConsulta = new JTextField();
     
     JScrollPane scrollPane = new JScrollPane();
     JScrollPane scrollPane1 = new JScrollPane();
+    JScrollPane scrollPane2 = new JScrollPane();
+    JScrollPane scrollPane3 = new JScrollPane();
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         IndiceAVL indiceAVL = new IndiceAVL();
-        indiceAVL.setBounds(300, 0, 700, 700);
+        indiceAVL.setBounds(300, 0, 1000, 700);
         indiceAVL.setTitle("Materias Cursores/AVL");
         indiceAVL.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         indiceAVL.setVisible(true);   
@@ -59,6 +68,7 @@ public class IndiceAVL extends JFrame implements ActionListener{
         c.add(botonIngresoEstudiante);
         c.add(botonIngresoMateria);
         c.add(botonConsultar);
+        c.add(botonDibujar);
         
         c.add(texto1);
         c.add(texto2);
@@ -67,31 +77,37 @@ public class IndiceAVL extends JFrame implements ActionListener{
         c.add(texto5);
         c.add(texto6);
         c.add(texto7);
+        c.add(texto8);
         
         c.add(codEst);
         c.add(nomEst);
         c.add(matEst);
         c.add(codMat);
         c.add(nomMat);
+        c.add(campoConsulta);
         
         c.add(scrollPane1);
+        c.add(scrollPane3);
         
         texto1.setBounds(70, 10, 200, 30);
-        texto2.setBounds(300, 10, 200, 30);
+        texto2.setBounds(400, 10, 200, 30);
         texto3.setBounds(5, 50, 200, 30);
         texto4.setBounds(5, 90, 200, 30);
         texto5.setBounds(5, 130, 200, 30);
-        texto6.setBounds(250, 50, 200, 30);
-        texto7.setBounds(250, 90, 200, 30);
+        texto6.setBounds(350, 50, 200, 30);
+        texto7.setBounds(350, 90, 200, 30);
+        texto8.setBounds(730, 105, 250, 30);
         
         codEst.setBounds(120, 55, 100, 20);
         nomEst.setBounds(120, 95, 100, 20);
         matEst.setBounds(70, 135, 150, 20);
         
-        codMat.setBounds(350, 55, 100, 20);
-        nomMat.setBounds(350, 95, 100, 20);
+        codMat.setBounds(450, 55, 100, 20);
+        nomMat.setBounds(450, 95, 100, 20);
         
-        botonCrear.setBounds(500, 120, 150, 30);
+        campoConsulta.setBounds(770,140,100,20);
+        
+        botonCrear.setBounds(660, 10, 320, 30);
         botonCrear.addActionListener(this);
         botonCrear.setBackground(Color.CYAN);
         
@@ -99,21 +115,33 @@ public class IndiceAVL extends JFrame implements ActionListener{
         botonIngresoEstudiante.addActionListener(this);
         botonIngresoEstudiante.setBackground(Color.GREEN);
         
-        botonIngresoMateria.setBounds(250, 170, 200, 30);
+        botonIngresoMateria.setBounds(350, 170, 200, 30);
         botonIngresoMateria.addActionListener(this);
         botonIngresoMateria.setBackground(Color.YELLOW);
         
-        botonConsultar.setBounds(500, 170, 150, 30);
+        botonConsultar.setBounds(660, 170, 320, 30);
         botonConsultar.addActionListener(this);
         botonConsultar.setBackground(Color.ORANGE);
+        
+        botonDibujar.setBounds(660, 50, 320, 30);
+        botonDibujar.addActionListener(this);
+        botonDibujar.setBackground(Color.blue);
         
         scrollPane.setBounds(2, 220, 2500, 2500);
         scrollPane.setPreferredSize(new Dimension(2500, 2500));  
         scrollPane.setBackground(Color.LIGHT_GRAY);
         
-        scrollPane1.setBounds(2, 220, 680, 430);
-        scrollPane1.setPreferredSize(new Dimension(680, 400));
+        scrollPane1.setBounds(2, 220, 650, 430);
+        scrollPane1.setPreferredSize(new Dimension(650, 430));
         scrollPane1.setBackground(Color.BLUE);
+        
+        scrollPane2.setBounds(660, 220, 600, 1000);
+        scrollPane2.setPreferredSize(new Dimension(600, 1000));  
+        scrollPane2.setBackground(Color.LIGHT_GRAY);
+        
+        scrollPane3.setBounds(660, 220, 320, 430);
+        scrollPane3.setPreferredSize(new Dimension(320, 430));
+        scrollPane3.setBackground(Color.BLUE);
         
     }
     
@@ -122,9 +150,70 @@ public class IndiceAVL extends JFrame implements ActionListener{
     
         if(e.getSource() == botonCrear){
             
+            arbol = new ArbolAVL();
             
+            try {
+                
+                materias = new Cursor();
+                
+            } catch (IOException ex) {
+                
+                Logger.getLogger(IndiceAVL.class.getName()).log(Level.SEVERE, null, ex);
+                
+            }
             
-        } else if (e.getSource() == botonConsultar){
+            materias.setRuta("materias.txt");
+            
+            try {
+                
+                estudiantes = new Cursor();
+            
+            } catch (IOException ex) {
+                
+                Logger.getLogger(IndiceAVL.class.getName()).log(Level.SEVERE, null, ex);
+            
+            }
+            
+            estudiantes.setRuta("estudiantes.txt");
+            
+        } else if(e.getSource() == botonIngresoEstudiante && !codEst.getText().equals("") && !nomEst.getText().equals("") && !matEst.getText().equals("")){
+           
+            String s = "";        
+            s = codEst.getText() + nomEst.getText() + matEst.getText();   
+            String listContenido[] = s.split(",");
+
+            try {
+
+                estudiantes.insertar(s.toCharArray());
+                String textoFichero[] = estudiantes.getNombre();   
+
+                for (int i = 0; i < textoFichero.length; i=i+3) {
+
+                    String ContenidoPalabra[] = textoFichero[i].split(",");
+                    System.out.println(ContenidoPalabra[0]);
+                    //arbol.insertar(Integer.parseInt(ContenidoPalabra[0]));
+                    System.out.println(textoFichero[i]);
+
+                }
+
+            }catch(IOException e1){
+
+                System.out.println("Datos de ingreso no validos");
+
+            }
+            
+        } else if(e.getSource() == botonIngresoMateria && !codMat.getText().equals("") && !nomMat.getText().equals("")) {
+                try {
+
+                    materias.insertar((codMat.getText() + nomMat.getText()).toCharArray());
+
+                }catch(IOException e2){
+
+                    System.out.println("Datos de ingreso no validos");
+
+                }
+            
+        } else if (e.getSource() == botonDibujar){
         
             scrollPane.removeAll();
          
@@ -132,17 +221,50 @@ public class IndiceAVL extends JFrame implements ActionListener{
             
             scrollPane1.setViewportView(scrollPane);
             
+        }else if (e.getSource() == botonConsultar && !campoConsulta.getText().equals("")){
+
+            try {
+
+                String[] textoFichero = estudiantes.getNombre(); 
+
+                for (int i = 0; i < textoFichero.length; i++) {
+
+                    String ContenidoPalabra[] = textoFichero[i].split(",");
+                    System.out.println(textoFichero[i]);
+
+                    if(campoConsulta.getText().equals(ContenidoPalabra[2])){
+
+                        String materiasFichero[] = materias.getNombre();
+
+                        for (int j = 0; j < materiasFichero.length; j++) {
+
+                            String numero[] = materiasFichero[j].split(",");
+                            if(ContenidoPalabra[2].equals(materiasFichero[j])){
+                                System.out.println(materiasFichero[j+1]);;
+                            }
+
+                        }
+
+                    }
+                }
+
+            }catch(Exception e3){
+
+                System.out.println("Datos de ingreso no validos");
+
+            }
+            
+            scrollPane2.removeAll();
+         
+            scrollPane2.repaint();
+            
+            scrollPane3.setViewportView(scrollPane2);
+            
         }
         
     }
 
 }
-    
-//        ArbolAVL arbol = new ArbolAVL();
-//        Cursor materias = new Cursor();
-//        materias.setRuta("materias.txt");
-//        Cursor estudiantes = new Cursor();
-//        estudiantes.setRuta("estudiantes.txt");
 //        Scanner sc = new Scanner(System.in);
 //        int p = 0;
 //
